1c62ed2e5b1b39384d153d11c9070cc2260c308d
chetanpatidsar04 <chetanpatidar04@gmail.com>
2023-03-16 15:00:18 +0530

    changes in models

--- store/models/customer.py
+++ store/models/customer.py
@@ -5,13 +5,13 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
-    mob_number = models.IntegerField(max_length=10)
+    mob_number = models.IntegerField()
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=200)
     address = models.CharField(max_length=500)
     city = models.CharField(default="Shajapur", max_length=50)
     state = models.CharField(default='Madhya Pradesh', max_length=50)
-    pincode = models.IntegerField(max_length=6)
+    pincode = models.IntegerField()
 
     def register(self):
         self.save()
--- store/models/orders.py
+++ store/models/orders.py
@@ -2,8 +2,7 @@ from email.headerregistry import Address
 from django.db import models
 from .product import Product
 from .customer import Customer
-import datetime
-
+from django.utils import timezone
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -11,8 +10,8 @@ class Order(models.Model):
     quantity = models.IntegerField(default=1)
     price = models.IntegerField()
     address = models.CharField(max_length=50, default="", blank=True)
-    mob_number = models.CharField(max_length=12, default="", blank=True)
-    date = models.DateTimeField(default=datetime.datetime.today())
+    mob_number = models.IntegerField(blank=False)
+    date = models.DateTimeField(default=timezone.now())
     status = models.BooleanField(default=False)
 
     def place_order(self):
